f7cdcbc1db6c331f87c17b51cd2be51aa0b971d7
GiuliaSossai <giulia.sossai@yahoo.it>
2021-12-21 08:14:36 +0100

    snack 3-4-5

---
+++ snack-04/index.php
@@ -0,0 +1,37 @@
+<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+<?php 
+function getRandom($min, $max, $nItems){
+   $myArr = [];
+
+   //verifico di poter avere tanti nItems quanti ne voglio
+   if(($max - $min) < $nItems){
+      $nItems = $max - $min;
+   };
+
+   while(count($myArr) < $nItems){
+      //estraggo numero random
+      $myNumber = rand($min, $max);
+      //lo pusho nel mio array se non è già presente
+      if(!in_array($myNumber, $myArr)){
+         $myArr[] = $myNumber;
+      };
+   };
+
+   return $myArr;
+};
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Snack 04</title>
+</head>
+<body>
+   <?php var_dump(getRandom(10, 50, 15)); ?>
+</body>
+</html>
