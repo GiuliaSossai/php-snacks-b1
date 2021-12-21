c38fcc1affad8c9f960fecb26e5ba92cc5e8772e
GiuliaSossai <giulia.sossai@yahoo.it>
2021-12-21 08:22:19 +0100

    snack 06

--- snack-06/index.php
+++ snack-06/index.php
@@ -1,6 +1,28 @@
 <!-- Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
 
 <?php
+$db = [
+   'teachers' => [
+      [
+         'name' => 'Michele',
+         'lastname' => 'Papagni'
+      ],
+      [
+         'name' => 'Fabio',
+         'lastname' => 'Forghieri'
+      ]
+   ],
+   'pm' => [
+      [
+         'name' => 'Roberto',
+         'lastname' => 'Marazzini'
+      ],
+      [
+         'name' => 'Federico',
+         'lastname' => 'Pellegrini'
+      ]
+   ]
+];
 
 ?>
 
@@ -13,6 +35,24 @@
    <title>Snack 06</title>
 </head>
 <body>
+   <div style="background-color: grey;">
+      <?php
+         for($i = 0; $i < count($db["teachers"]); $i++){
+            echo $db["teachers"][$i]["name"];
+            echo $db["teachers"][$i]["lastname"]. "<br>";
+         };
+      ?>
+   </div>
    
+   <br>
+
+   <div style="background-color: green;">
+      <?php
+         for($i = 0; $i < count($db["pm"]); $i++){
+            echo $db["pm"][$i]["name"];
+            echo $db["pm"][$i]["lastname"]. "<br>";
+         };
+      ?>
+   </div>
 </body>
 </html>
